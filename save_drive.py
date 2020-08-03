e8efb27d8cbc8ca399f322a4271854f11b534c65
= <=>
2020-08-03 03:46:14 +0000

    Refactoring

--- save_drive.py
+++ save_drive.py
@@ -1,12 +1,17 @@
 from IPython import get_ipython
 from time import sleep
+from pathlib import Path
 
-s1 = "/content/clouderizer/melanoma/code"
-d1 = "/content/drive/My\ Drive/clouderizer/melanoma/code"
-s2 = "/content/clouderizer/melanoma/out/2020.test_effb0_rr_oc"
-d2 = "/content/drive/My\ Drive/clouderizer/melanoma/out/2020.test_effb0_rr_oc"
+BASE_PATH = Path.cwd().expanduser().parent
+DRIVE = Path("/content/drive/My\ Drive/clouderizer/melanoma")
+
+s1 = BASE_PATH / "code"
+d1 = DRIVE / "code"
+s2 = BASE_PATH / "out"
+d2 = DRIVE /  "out"
 
 while True:
     #get_ipython().system(f"rclone sync {s1} {d1} -q --fast-list")
-    get_ipython().system(f"rclone sync {s2} {d2} --fast-list -v")
+    command = f'rclone copy {s2} {d2} --fast-list -v'
+    get_ipython().system(command)
     sleep(360)
